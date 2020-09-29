0416cd0b30c77415c18cddffcd9449101c7e43ca
jcs <jaredscruggs@outlook.com>
2020-09-29 17:15:25 -0400

    added clothingtype model

---
+++ parakeet/parakeet/Models/ClothingType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parakeet.Models
+{
+    public class ClothingType
+    {
+        public int ClothingTypeId { get; set; }
+        public string type { get; set; }
+
+        public String UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
