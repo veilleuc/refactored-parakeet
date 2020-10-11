8e06713690e419620215dd96402409813d057ce4
Alix Bryant <axbryant@email.uscupstate.edu>
2020-10-11 12:58:56 -0400

    Product page search and buttons

---
+++ parakeet/parakeet/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using parakeet.Models;
+using Microsoft.EntityFrameworkCore;
+using parakeet.Data;
+
+namespace parakeet.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            Design[] designs = _context.Designs.ToArray();
+
+            // Display designs 
+
+            return View(designs);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
