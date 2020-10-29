27e722d6914c672c50d0afaf44dd965332456b46
jcs <jaredscruggs@outlook.com>
2020-10-28 20:54:17 -0400

    updated cart and order history UI

--- parakeet/parakeet/Startup.cs
+++ parakeet/parakeet/Startup.cs
@@ -28,7 +28,7 @@ namespace parakeet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CharlesConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("JaredConnection")));
             services.AddIdentity<ApplicationUser, IdentityRole>()
                     .AddEntityFrameworkStores<ApplicationDbContext>()
                     .AddDefaultUI()
